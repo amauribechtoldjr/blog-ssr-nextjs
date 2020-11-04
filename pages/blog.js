1ec5c3739ee384efafbae39be4020ae57eddfa7f
Amauri Bechtold Junior <amauri.bechtold@grupoanga.com>
2020-11-04 17:14:33 -0300

    Adicionando imagens do layout padrão

--- pages/blog.js
+++ pages/blog.js
@@ -3,7 +3,7 @@ import BaseLayout from "../components/layouts/BaseLayout";
 export default function Blog() {
   return (
     <BaseLayout>
-      <h1>{`Eu sou a página Blog`}</h1>
+      <h2>{`Eu sou a página Blog`}</h2>
     </BaseLayout>
   );
 }
